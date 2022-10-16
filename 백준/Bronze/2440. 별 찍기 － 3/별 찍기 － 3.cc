1e6003e74f2ec53f3a248c657a032744a88decf8
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 03:35:37 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+
+	int N;
+	scanf("%d", &N);
+	for (int i = 0;i <N;i++)
+	{		
+		for (int j = N-i;j > 0;j--)
+			printf("*");
+	printf("\n");
+	}
+	return 0;
+}
