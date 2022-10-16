487c0d7402d3760e800b868a35361bb72a583fcc
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:11:56 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+
+	int a[3];
+	scanf("%d %d %d", &a[0], &a[1], &a[2]);
+	if (a[0] + a[1] + a[2] >= 100)printf("OK");
+	else {
+		if (a[0] <= a[1] && a[0] <= a[2])printf("Soongsil");
+		if (a[1] <= a[0] && a[1] <= a[2])printf("Korea");
+		if (a[2] <= a[1] && a[2] <= a[0])printf("Hanyang");
+	}
+	return 0;
+}
