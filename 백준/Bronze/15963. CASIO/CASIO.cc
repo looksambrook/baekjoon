7305e9674325538f66603e619d47a1760ea01709
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:14:06 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a == b)printf("1");
+	else printf("0");
+	return 0;
+}
