4863cf86fb7396a46b7f5b8c6c7a715c1486bba9
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-17 04:07:02 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+
+	char a;
+	scanf("%c", &a);
+	if (a == 'N' || a == 'n')printf("Naver D2");
+	else printf("Naver Whale");
+	return 0;
+}
