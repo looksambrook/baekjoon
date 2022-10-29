edf70aebfc5010f85483d1eaaa3031b8a054f6d2
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-29 11:50:52 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+
+	char san[100];
+	
+	scanf("%s", san);
+
+	for (int i = 0;san[i];i++)
+	{
+		printf("%c", san[i]);
+		if ((i + 1) % 10 == 0)
+			printf("\n");
+	}
+	
+	return 0;
+}
