a7b5a62a3c5a8e3409171522386ff50681f393e7
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-29 11:41:35 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+
+	int count = 0, sum = 0, T;
+	char san[51];
+
+	scanf("%d", &T);
+	for (int i = 0;i < T;i++)
+	{
+		count = 0;
+		sum = 0;
+
+		scanf("%s", san);
+		for (int j = 0;san[j];j++)
+		{
+			if (san[j] == '(')count++;
+			else count--;
+			if(count<0) sum = -100;
+		}
+		if (sum != -100)sum = count;
+		if (sum == 0)printf("YES\n");
+		else printf("NO\n");
+	}
+	
+	return 0;
+}
