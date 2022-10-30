b21bc51ca5b98c3c06e74e9d5fb8a2150f6535a7
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-10-31 00:29:17 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    int lon,lo;
+    char parlin[99999];
+
+    while (1) {
+        scanf("%s", parlin);
+        if (parlin[0] == '0')break;
+        lon = strlen(parlin);
+        if (lon % 2 == 0)lo = lon / 2;
+        else lo = lon / 2 + 1;
+        for (int i = 0;i <= lo;i++)
+        {
+            if (parlin[i] == parlin[lon - i - 1])continue;
+            else {
+                printf("no\n");
+                lon = 0;
+                break;
+            }
+        }
+        if (lon == 0)continue;
+        else printf("yes\n");
+    }
+    return 0;
+}
