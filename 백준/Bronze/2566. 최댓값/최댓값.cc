de83ce927aec31903dcfa124c72bf5aea8356d96
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-11-07 22:49:26 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+
+    int i = 0, j, max = -1, a = 0, b = 0;
+    char san[10][10];
+
+    while (i < 9)
+    {
+        for (j = 0;j < 9;j++)
+        {
+            scanf("%d", &san[i][j]);
+            getchar();
+            if (max < (int)san[i][j]) {
+                max = (int)san[i][j];
+                a = i + 1, b = j + 1;
+            }
+
+        }
+
+        i++;
+    }
+
+    printf("%d", max);
+    puts("");
+    printf("%d %d", a, b);
+
+    return 0;
+}
