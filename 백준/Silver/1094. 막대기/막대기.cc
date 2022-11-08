b81ca3dac1d837fbabbffaff18d40969bf592e04
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-11-08 23:44:22 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int stick(int n, int k,int sum)
+{
+	if (n - k > 0) {
+		sum++;
+		n = n - k;
+		k = k / 2;
+		return stick(n, k, sum);
+	}
+	else if (n == k) {
+		sum++;
+		return sum;
+	}
+	else {
+		k = k / 2;
+		return stick(n, k, sum);
+	}
+}
+
+int main(void)
+{
+	int n, sum = 0, k = 64;
+
+	scanf("%d", &n);
+	printf("%d", stick(n, k, sum));
+
+	return 0;
+}
