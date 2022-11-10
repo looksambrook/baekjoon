9060fb7e4d05c6d03dd0d97033f40f3a669bc945
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-11-10 23:10:29 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int L;
+	scanf("%d", &L);
+	if (L % 5 == 0)printf("%d", L / 5);
+	else printf("%d", L / 5 + 1);
+
+	
+	return 0;
+}
