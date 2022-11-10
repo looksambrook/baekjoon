fd3ac6d66d303f96f97f9d9b0cb637cdaab58501
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-11-10 23:39:42 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int a, b;
+
+	while (1)
+	{
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0)break;
+		else if (a <= b)printf("No\n");
+		else printf("Yes\n");
+	}
+	
+	return 0;
+}
