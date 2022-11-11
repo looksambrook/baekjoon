a22342f112f4ea7b9b93f72091e40f5559bd02f5
looksambrook <104709953+looksambrook@users.noreply.github.com>
2022-11-11 23:48:20 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(void) {
+	//int N, M;
+	//char num[50][50];
+
+	//scanf("%d %d", &N, &M);
+	//for (int i = 0;i < N;i++)
+	//{
+	//	for (int j = 0;j < M;j++)
+	//	{
+	//		scanf("%d", &num[i][j]);
+	//	}
+	//}//1051
+
+	printf("파이팅!!");
+	return 0;
+}
