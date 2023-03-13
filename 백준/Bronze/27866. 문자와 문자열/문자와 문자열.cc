f4451c623f86d3102250132dc6199ff2a81fd788
looksambrook <104709953+looksambrook@users.noreply.github.com>
2023-03-14 08:49:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>	
+
+int main() {
+	char str[1000];
+	int i;
+
+	scanf("%s", str);
+	scanf("%d", &i);
+
+	printf("%c", str[i - 1]);
+
+	return 0;
+}
