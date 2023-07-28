c8363d2a2860f73d92a552e8d0ec5d91f59c687b
looksambrook <104709953+looksambrook@users.noreply.github.com>
2023-07-28 15:01:23 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,44 +1,43 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<vector>
 
 using namespace std;
-#define MAX 101
 
-int n,m;
-int cnt=0;
-int arr[MAX][MAX];
-bool visited[MAX]={0,};
-queue<int> q;
+const int MAX = 101;
 
-void bfs(int v) {
-    q.push(v);
-    visited[v]=true;
+int cnt = 0;
+vector<int> myGraph[MAX];
+bool visited[MAX];
 
-    while(!q.empty()) {
-        v = q.front();
-        q.pop();
+void DFS(int x) {
+	visited[x] = true;
 
-        for(auto i=1;i<=n;i++) {
-            if(arr[v][i]==1 && visited[i]==0) {
-                q.push(i);
-                visited[i]=true;
-                cnt++;
-            }
-        }
-    }
+	for (int i = 0;i < myGraph[x].size();i++) {
+		int y = myGraph[x][i];
+
+		if (visited[y] == false) {
+			cnt++;
+			DFS(y);
+		}
+	}
+	if(x==1)
+		cout << cnt;
 }
 
 int main() {
-    cin >> n >> m;
+	int N, M; //N:정점개수, M:간선개수
 
-    for(auto i=0;i<m;i++) {
-        int x,y;
-        cin >> x >> y;
-        arr[x][y]=1;
-        arr[y][x]=1;
-    }
+	cin >> N >> M;
 
-    bfs(1);
+	for (int i = 0;i < M;i++) {
+		int u, v;
+		cin >> u >> v;
 
-    cout << cnt;
+		myGraph[u].push_back(v);
+		myGraph[v].push_back(u);
+	}
+
+	DFS(1);
+	
+	return 0;
 }
