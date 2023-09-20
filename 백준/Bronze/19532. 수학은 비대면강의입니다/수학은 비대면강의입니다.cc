5f0286117bec87c81fcd5b79ea8303aa16ab3f59
looksambrook <104709953+looksambrook@users.noreply.github.com>
2023-09-21 00:12:49 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int a, b, c, d, e, f;
+	int x, y;
+
+	cin >> a >> b >> c >> d >> e >> f;
+
+	if (a == 0) {
+		y = c / b;
+		x = (f - e * y) / d;
+	}
+	else {
+		y = (c * d - a * f) / (b * d - a * e);
+		x = (c - b * y) / a;
+	}
+
+	cout << x << " " << y;
+
+	return 0;
+}
