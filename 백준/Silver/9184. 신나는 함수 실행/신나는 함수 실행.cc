2aa5a8e142a7a3f304f301df031c69cfe03b0861
looksambrook <104709953+looksambrook@users.noreply.github.com>
2023-10-07 10:18:34 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 408 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -1,28 +1,36 @@
-#include <iostream>
+#include<iostream>	
+
 using namespace std;
-int arr[21][21][21];
-int w(int a, int b, int c) {
-	if (a <= 0 || b <= 0 || c <= 0) {
+
+int save[21][21][21];
+
+int recursion(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)
 		return 1;
+	if(a>20||b>20||c>20) {
+		a = 20;b = 20;c = 20;
+		return recursion(a, b, c);
 	}
-	if (a > 20 || b > 20 || c > 20) {
-		return w(20, 20, 20);
-	}
-	if (arr[a][b][c] != 0) return arr[a][b][c];
-	if (a < b && b < c) {
-		return arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-	}
-	else {
-		return arr[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-	}
+	else if (save[a][b][c] != 0)return save[a][b][c];
+	else if (a < b && b < c)
+		return save[a][b][c] = (recursion(a, b, c - 1) + recursion(a, b - 1, c - 1) - recursion(a, b - 1, c));
+	else if (a == 1)
+		return 2;
+	else
+		return save[a][b][c] = (recursion(a - 1, b, c) + recursion(a - 1, b - 1, c) + recursion(a - 1, b, c - 1) - recursion(a - 1, b - 1, c - 1));
 }
 
 int main() {
 	int a, b, c;
 	while (1) {
-			cin >> a >> b >> c;
-			if (a == -1 && b == -1 && c == -1) break;
-			cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+		cin >> a >> b >> c;
+
+		if (a == -1 && b == -1 && c == -1)
+			break;
 		
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << recursion(a, b, c) << "\n";
 	}
+
+
+	return 0;
 }
